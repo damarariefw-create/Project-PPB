386cd84a0bf578ed7b3e7fd8d33e6e6c6abfc161
Damar Arief W <damarariefwtjksn@gmail.com>
2026-08-12 13:18:09 +0700

    cp01

--- app/src/main/java/com/example/projectppb/MainActivity.java
+++ app/src/main/java/com/example/projectppb/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.projectppb;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText phone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        phone = findViewById(R.id.phone);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void submit(View view) {
+    EditText input = findViewById(R.id.phone);
+        Toast.makeText(this, "Nomor telepon: " + phone.getText(), Toast.LENGTH_SHORT).show();
+        input.setText("");
     }
 }
